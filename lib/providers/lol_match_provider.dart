8d2a17877a66d1202ae91958a03d2a15a5c9957d
이우진 <lwj1370@naver.com>
2022-11-23 16:54:27 +0900

    [수정] async 데이터 화면 파싱 1. future 데이터 화면 출력 2. log 추가

--- lib/providers/lol_match_provider.dart
+++ lib/providers/lol_match_provider.dart
@@ -1,22 +1,28 @@
 
 import 'dart:convert';
-
+import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:lolprosapp/models/match_model.dart';
-// import 'package:lolprosapp/models/participant/gamer_model.dart';
 
 Future<GameMatch> lolGamer(String gamer) async {
 
-  String url = 'http://127.0.0.1:8000/user/';
+  String url = 'http://127.0.0.1:8000/lol/match-api';
   GameMatch gameMatch = GameMatch.empty();
+  Map<String, String> body = new Map<String, String>();
+  body['nickname'] = gamer;
+
   final http.Response response;
   try {
-    response = await http.post(Uri.parse(url)).timeout(const Duration(seconds: 30),);
+    response = await http.post(
+      Uri.parse(url),
+      body: body
+      ).timeout(const Duration(seconds: 30),
+    );
 
     gameMatch = GameMatch.fromJson(jsonDecode(response.body));
-
+    log('Result : ${gameMatch.toString()}');
   } catch(e) {
-    print('Error Occurred $e');
+    log('Error Occurred $e');
   }
 
   return gameMatch;
--- lib/screens/gamer_screen.dart
+++ lib/screens/gamer_screen.dart
@@ -1,37 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:lolprosapp/models/match_model.dart';
 import 'package:lolprosapp/providers/lol_match_provider.dart';
+import 'dart:developer';
 
-class GamerScreen extends StatelessWidget {
-  GamerScreen({Key? key, required this.gamerName}) : super(key: key);
-
+class GamerScreen extends StatefulWidget {
   final String gamerName;
-  var lolResponse;
-  GameMatch gameMatch = GameMatch.empty();
+  const GamerScreen({Key? key, required this.gamerName}) : super(key: key);
+
+  @override
+  State<GamerScreen> createState() => _GamerScreenState();
+}
+
+class _GamerScreenState extends State<GamerScreen> {
+  late final Future<GameMatch> futureGameMatch;
 
   @override
   void initState() {
-    lolResponse = lolGamer(gamerName);
 
-    lolResponse.then((value) => {
-      gameMatch = value
-    });
+    // widget은 _GamerScreenState의 상위 GameScreen을 의미함
+    log('Requested Gamer Nickname : ${widget.gamerName}');
+    futureGameMatch = lolGamer(widget.gamerName);
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              children: [
+    return Container(
+      child: Scaffold(
+        body: Container(
+          child: FutureBuilder(
+            future: futureGameMatch,
+            builder: (context, snapshot) {
+              if(snapshot.connectionState == ConnectionState.done) {
+                GameMatch gameMatch = snapshot.data as GameMatch;
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(gameMatch.matchId)
+                      ],
+                    ),
+                  ],
+                );
+              }
 
-              ],
-            ),
-          ],
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            })
         ),
-      ),
+      )
     );
   }
 }
+
